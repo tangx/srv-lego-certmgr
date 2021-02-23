4f95f000a791a5e4bf3cc5c46e7633b56c6d3301
tangx <uyinn@live.com>
2021-02-23 14:03:45 +0800

    fix: public response

--- pkg/httpresponse/resp.go
+++ pkg/httpresponse/resp.go
@@ -6,13 +6,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type response struct {
+type Response struct {
 	Code  int
 	Data  interface{}
 	Error string
 }
 
-func RespDefault(code int, data interface{}, err error) response {
+func RespDefault(code int, data interface{}, err error) Response {
 	var msg string
 	if err == nil {
 		msg = ""
@@ -20,7 +20,7 @@ func RespDefault(code int, data interface{}, err error) response {
 		msg = err.Error()
 	}
 
-	return response{
+	return Response{
 		Code:  code,
 		Data:  data,
 		Error: msg,
@@ -32,7 +32,7 @@ func StatusDefault(c *gin.Context, code int, data interface{}, err error) {
 	c.JSON(code, resp)
 }
 
-func RespOK(data interface{}) response {
+func RespOK(data interface{}) Response {
 	return RespDefault(0, data, nil)
 }
 func StatusOK(c *gin.Context, data interface{}) {
@@ -40,7 +40,7 @@ func StatusOK(c *gin.Context, data interface{}) {
 	c.JSON(http.StatusOK, resp)
 }
 
-func RespBadRequest(err error) response {
+func RespBadRequest(err error) Response {
 	return RespDefault(400, nil, err)
 }
 
@@ -49,7 +49,7 @@ func StatusBadRequest(c *gin.Context, err error) {
 	c.JSON(http.StatusBadRequest, resp)
 }
 
-func RespNotFound(err error) response {
+func RespNotFound(err error) Response {
 	return RespDefault(404, nil, err)
 }
 

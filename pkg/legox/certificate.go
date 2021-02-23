5b0f71f52d16ab49f0f5f971e9b4fa883c76f5d7
tangx <uyinn@live.com>
2021-02-23 13:55:37 +0800

    style: 证书时间 json 格式

--- pkg/legox/certificate.go
+++ pkg/legox/certificate.go
@@ -11,12 +11,12 @@ import (
 type Certificate struct {
 	Domain string `json:"domain,omitempty"`
 	// acem 下载证书地址
-	CertURL       string `json:"cert_url,omitempty"`
-	CertStableURL string `json:"cert_stable_url,omitempty"`
-	PrivateKey    string `json:"private_key,omitempty"`
-	Certificate   string `json:"certificate,omitempty"`
-	NotAfter      time.Time
-	NotBefore     time.Time
+	CertURL       string    `json:"cert_url,omitempty"`
+	CertStableURL string    `json:"cert_stable_url,omitempty"`
+	PrivateKey    string    `json:"private_key,omitempty"`
+	Certificate   string    `json:"certificate,omitempty"`
+	NotAfter      time.Time `json:"not_after,omitempty"`
+	NotBefore     time.Time `json:"not_before,omitempty"`
 }
 
 // SetValidationTime 填充证书有效时间
